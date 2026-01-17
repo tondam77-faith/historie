6f36e78279940acc6c9b0c8de46455594432397b
tondam77-faith <tondam77@gmail.com>
2026-01-17 16:50:11 +0100

    Verze 34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'historie-srs-v33'; // ZMĚNIT PŘI KAŽDÉM UPDATE (v33, v34...)
+const CACHE_NAME = 'historie-srs-v34'; // ZMĚNA NA v34
 const ASSETS = [
   './',
   './index.html',
@@ -6,34 +6,26 @@ const ASSETS = [
   './icon.png'
 ];
 
-// Instalace - stáhne nové soubory
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // Donutí nový SW aktivovat se ihned
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Aktivace - SMAŽE STAROU VERZI APLIKACE (ale data v DB nechá!)
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log('Mažu starou cache:', key);
             return caches.delete(key);
           }
         })
       );
     })
   );
-  return self.clients.claim(); // Převezme kontrolu nad otevřenými stránkami
+  return self.clients.claim();
 });
 
-// Fetch - slouží soubory z cache nebo sítě
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
