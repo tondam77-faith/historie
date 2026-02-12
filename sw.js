5b3277007628b781d9644f4a8f0bf34e7f677d4d
tondam77-faith <tondam77@gmail.com>
2026-02-12 21:51:04 +0100

    verze 3.0

--- sw.js
+++ sw.js
@@ -1,17 +1,20 @@
-const CACHE_NAME = 'historie-srs-v1.5';
+const CACHE_NAME = 'historie-srs-v3.0-FINAL';
 const ASSETS = [
   './',
   './index.html',
+  './editor.html',
   './manifest.json',
   './history.png',
   './favicon.ico'
 ];
 
+// Instalace a stažení souborů do paměti
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
+// Aktivace a vymazání staré paměti (tohle opraví chyby)
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
@@ -27,6 +30,7 @@ self.addEventListener('activate', (e) => {
   return self.clients.claim();
 });
 
+// Obsluha požadavků (offline režim)
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
